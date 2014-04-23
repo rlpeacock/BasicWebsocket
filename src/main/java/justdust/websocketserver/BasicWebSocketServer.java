dcbc7fee5ca0070ca3a5db6ab4e63421790b929a
JustDust <rlpeacock@gmail.com>
2014-04-22 21:56:58 -0400

    added BasicWebSocket

--- src/main/java/justdust/websocketserver/BasicWebSocketServer.java
+++ src/main/java/justdust/websocketserver/BasicWebSocketServer.java
@@ -5,6 +5,7 @@ import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocketServlet;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Simple webserver listening on a web socket and sending back whatever it receives.
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 public class BasicWebSocketServer {
 
     public static void start() throws Exception {
-        org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server(8081);
+        org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server(8888);
         ServletContextHandler contextHandler = new ServletContextHandler(server, "/", true, false);
         contextHandler.addServlet(BasicWebSocketServlet.class, "/ws");
         server.start();
@@ -23,6 +24,31 @@ public class BasicWebSocketServer {
         @Override
         public WebSocket doWebSocketConnect(HttpServletRequest httpServletRequest, String s) {
             return new BasicWebSocket();
+        }
+    }
+
+    public static class BasicWebSocket implements WebSocket.OnTextMessage {
+
+        private Connection connection;
+
+        @Override
+        public void onOpen(Connection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public void onClose(int i, String s) {
+            System.out.println("Closing");
+        }
+
+        @Override
+        public void onMessage(String s) {
+            System.out.println("Got message: " + s);
+            try {
+                connection.sendMessage("Got " + s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
